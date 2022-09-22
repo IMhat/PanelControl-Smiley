5c3a33f2bebf7cd0bbf4d52ed556bedec6726462
Matias <98600292+Matiaslarragnetta2022@users.noreply.github.com>
2022-09-22 18:13:30 -0300

    Update task_card.dart

--- lib/app/utils/widgets/task_card.dart
+++ lib/app/utils/widgets/task_card.dart
@@ -116,11 +116,14 @@ class TaskCard extends StatelessWidget {
 //   }
 // }
 
+
 class _TaskDetails extends StatefulWidget {
   final String? title;
-  final String? subTitle;
+  final String? type;
+  final String? priority;
+  //final String? subTitle;
 
-  const _TaskDetails({this.title, this.subTitle});
+  const _TaskDetails({this.title, this.type, this.priority});
 
   @override
   State<_TaskDetails> createState() => _TaskDetailsState();
@@ -134,64 +137,119 @@ class _TaskDetailsState extends State<_TaskDetails> {
       onPrimary: Colors.white);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      width: 280,
-      height: 300,
-      decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 231, 170, 209),
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey[850]!.withOpacity(0.29),
-              offset: const Offset(-10, 10),
-              blurRadius: 10,
-            )
-          ]),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            widget.title.toString(),
-            style: const TextStyle(
-                fontSize: 20,
-                color: Color.fromARGB(255, 7, 0, 0),
-                fontWeight: FontWeight.bold),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-          Text(
-            widget.subTitle.toString(),
-            style: const TextStyle(
-                fontSize: 15, color: Color.fromARGB(255, 9, 0, 0)),
-          ),
-          const SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // children: [
-            //   ElevatedButton(
-            //     style: elevatedButtonStyle,
-            //     onPressed: () {
-            //       Navigator.pushNamed(
-            //         context,
-            //         'TaskDetail',
-            //       );
-            //     },
-            //     child: const Text("Ver"),
-            //   ),
-            //   ElevatedButton(
-            //     style: elevatedButtonStyle,
-            //     onPressed: () {
-            //       Navigator.pushNamed(
-            //         context,
-            //         'tasksPost',
-            //       );
-            //     },
-            //     child: const Text("Empezar"),
-            //   ),
-            // ],
-          ),
-        ],
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: 320,
+        height: 300,
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 147, 94, 237),
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[850]!.withOpacity(0.29),
+                offset: const Offset(-10, 10),
+                blurRadius: 10,
+              )
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 5),
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 255, 251, 251)),
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+                Text(
+                  widget.title.toString(),
+                  style: const TextStyle(
+                      fontSize: 20,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Icon(Icons.menu)
+              ],
+            ),
+
+            SizedBox(
+              height: 50,
+              child: Text(
+                widget.priority.toString(),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 5),
+                  width: 130,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 255, 251, 251)),
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Text(
+                    widget.type.toString(),
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 0, 0)),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage(ImageRasterPath.avatar1),
+                )
+              ],
+            ),
+            // Text(
+            //   widget.subTitle.toString(),
+            //   style: const TextStyle(
+            //       fontSize: 15, color: Color.fromARGB(255, 9, 0, 0)),
+            // ),
+            const SizedBox(
+              height: 40,
+              child: Icon(Icons.comment_bank),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // children: [
+              //   ElevatedButton(
+              //     style: elevatedButtonStyle,
+              //     onPressed: () {
+              //       Navigator.pushNamed(
+              //         context,
+              //         'TaskDetail',
+              //       );
+              //     },
+              //     child: const Text("Ver"),
+              //   ),
+              //   ElevatedButton(
+              //     style: elevatedButtonStyle,
+              //     onPressed: () {
+              //       Navigator.pushNamed(
+              //         context,
+              //         'tasksPost',
+              //       );
+              //     },
+              //     child: const Text("Empezar"),
+              //   ),
+              // ],
+            ),
+          ],
+        ),
       ),
     );
   }
